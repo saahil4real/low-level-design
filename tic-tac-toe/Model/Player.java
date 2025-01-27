670ce76b5c4432ef9e42cd90d5f202789b8efdec
saahil4real <60621302+saahil4real@users.noreply.github.com>
2025-01-27 23:23:13 +0530

    Tic Tac Toe LLD code

---
+++ tic-tac-toe/Model/Player.java
@@ -0,0 +1,12 @@
+package LLDTicTacToe.Model;
+
+public class Player {
+
+    public String name;
+    public PlayingPiece playingPiece;
+
+    public Player(String name, PlayingPiece playingPiece) {
+        this.name = name;
+        this.playingPiece = playingPiece;
+    }
+}
