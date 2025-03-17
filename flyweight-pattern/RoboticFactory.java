08e7b2d771660ddb434931d4d9103c2ea5fe9f0a
saahil4real <60621302+saahil4real@users.noreply.github.com>
2025-03-18 00:17:30 +0530

    Flyweight Pattern Implementation

---
+++ flyweight-pattern/RoboticFactory.java
@@ -0,0 +1,32 @@
+package LowLevelDesign.DesignPatterns.FlyWeightPattern;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class RoboticFactory {
+
+    private static Map<String, IRobot> roboticObjectCache = new HashMap<>();
+
+    public static IRobot createRobot(String robotType){
+
+        if(roboticObjectCache.containsKey(robotType)){
+            return roboticObjectCache.get(robotType);
+        }
+        else {
+            if(Objects.equals(robotType, "HUMANOID")){
+                Sprites humanoidSprite = new Sprites();
+                IRobot humanoidObject = new HumanoidRobot(robotType, humanoidSprite);
+                roboticObjectCache.put(robotType, humanoidObject);
+                return humanoidObject;
+            }
+            else if(Objects.equals(robotType, "ROBOTICDOG")){
+                Sprites roboticDogSprite = new Sprites();
+                IRobot roboticDogObject = new RoboticDog(robotType, roboticDogSprite);
+                roboticObjectCache.put(robotType, roboticDogObject);
+                return roboticDogObject;
+            }
+        }
+        return null;
+    }
+}
